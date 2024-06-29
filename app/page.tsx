cc9507932315746c3d0462ac379815db141a7857
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-30 00:38:42 +0100

    Feat: Finished UI:

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { Ojuju } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 
 const ojuju = Ojuju({ subsets: ["latin"], weight: "800" });
 
@@ -31,13 +33,45 @@ const Home = () => {
         />
       </section>
 
-      <section className="flex flex-col items-start md:justify-center">
+      <section className="flex flex-col items-start md:justify-center md:p-2">
         <div className="md:block hidden mb-5">
           <HeaderText />
         </div>
-        <section className="w-full flex gap-4 ">
-          <Input type="text" placeholder="Your Name" className="p-8" />
-          <Input type="email" placeholder="Your Email" />
+        <section className="w-full md:p-0 p-2">
+          <section className="w-full flex md:flex-row flex-col items-center gap-x-5 ">
+            <div className="w-full md:my-0 my-2">
+              <Label htmlFor="yourName" className="text-base">
+                What's your name?
+              </Label>
+              <Input
+                type="text"
+                placeholder="Enter your name"
+                className="p-8 my-2"
+              />
+            </div>
+            <div className="w-full md:my-0 my-2">
+              <Label htmlFor="yourName" className="text-base">
+                Enter your crush's name?
+              </Label>
+              <Input
+                type="text"
+                placeholder="Enter your crush's name"
+                className="p-8 my-2"
+              />
+            </div>
+          </section>
+
+          <div className="w-full my-5">
+            <Button
+              className={
+                (ojuju.className,
+                "font-semibold w-full p-9 text-2xl border-[1px] flex items-center justify-center")
+              }
+              variant={"secondary"}
+            >
+              Play ❣️
+            </Button>
+          </div>
         </section>
       </section>
     </section>
