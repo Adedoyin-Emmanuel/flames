7f6d809411678f5871868756a6093bf764931ae5
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-29 23:54:51 +0100

    Feat: Added shadcn label

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
 import { Ojuju } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Input } from "@/components/ui/input";
 
 const ojuju = Ojuju({ subsets: ["latin"], weight: "800" });
 
@@ -21,26 +21,24 @@ const Home = () => {
   return (
     <section className="w-screen h-screen grid md:grid-cols-2 justify-center p-2 overflow-x-hidden">
       <section className="flex flex-col items-center justify-center">
-        <div className="md:hidden mb-2">
+        <div className="md:hidden mb-5">
           <HeaderText />
         </div>
         <img
           src="/assets/giving-flowers.jpg"
           alt="Giving flowers"
-          className=" w-full max-w-md"
+          className="w-full max-w-md"
         />
       </section>
 
-      <section className="flex flex-col items-start justify-center">
+      <section className="flex flex-col items-start md:justify-center">
         <div className="md:block hidden mb-5">
           <HeaderText />
         </div>
-        <p className="text-gray-700">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dolorem,
-          suscipit distinctio voluptates beatae ut! Quia delectus, eveniet est
-          error in sed consequuntur consectetur id, recusandae, ipsa officia.
-          Est, dicta.
-        </p>
+        <section className="w-full flex gap-4 ">
+          <Input type="text" placeholder="Your Name" className="p-8" />
+          <Input type="email" placeholder="Your Email" />
+        </section>
       </section>
     </section>
   );
